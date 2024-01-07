e7c7582e23258d5336668bf0b048813c949f9cd5
kuipid01 <kuipid01@gmnail.com>
2024-01-07 14:27:49 +0100

    added new changes

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -13,10 +13,15 @@ import { IoIosMenu } from "react-icons/io";
 import Alltransactions from "../comp/Alltransactions";
 import BarChart from "../comp/Bar";
 import { FiActivity } from "react-icons/fi";
+import { FaPlus } from "react-icons/fa6";
+import "react-modern-calendar-datepicker/lib/DatePicker.css";
+import { Calendar, DayValue } from "react-modern-calendar-datepicker";
+import { useState } from "react";
 const Dashboard = () => {
+  const [selectedDay, setSelectedDay] = useState<DayValue | null>(null);
   return (
-    <div className="bg-primary-500/60 relative text-white  w-full min-h-screen flex  ">
-      <div className=" hidden sticky shrink-0 h-screen top-0 md:flex py-10 shadow-2xl bg-white text-black rounded-r-[15px] flex-col justify-between px-4  left sm:w-[30%] md:w-[20%]">
+    <div className="bg-primary-500/60 overflow-hidden overflow-y-scrolls relative text-white  w-full  h-fit flex  ">
+      <div className="hidden sticky top-0 h-screen md:flex py-10 shadow-2xl bg-white text-black rounded-r-[15px] flex-col justify-between px-4 left sm:w-[30%] md:w-[20%]">
         <ul className=" flex flex-col gap-5">
           <li
             className={`  cursor-pointer hover:text-secondary-500 transition-all   text-[20px]   flex items-center gap-3`}
@@ -70,7 +75,7 @@ const Dashboard = () => {
       </div>
       <IoIosMenu className="absolute cursor-pointer mb-3 top-2 left-2 text-lg" />
       <div
-        className={`mt-6 flex-1 px-5 py-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6`}
+        className={`mt-6 h-fit flex-1 px-5 py-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6`}
       >
         <div className="bg-white h-[250px]  rounded-[20px]  border-white">
           <Alltransactions />
@@ -81,20 +86,43 @@ const Dashboard = () => {
         >
           <div className=" text-black px-2 mt-2 rounded-t-[20px]  shadow-sm py-3 flex sticky top-0 bg-white  items-center justify-between">
             <h1 className=" font-bold text-[20px]">Visualizations</h1>
-            <FiActivity  className="font-extralight text-gray-500 text-normal" />
+            <FiActivity className="font-extralight text-gray-500 text-normal" />
           </div>
           <BarChart titleOfText="Expenses vs Income" />
           <BarChart titleOfText="Expenses vs income" />
           <BarChart titleOfText="Expenses vs income" />
           <BarChart titleOfText="Expenses vs income" />
         </div>
+        <div className=" overflow-hidden bg-white relative h-[250px]  flex flex-col justify-evenly  rounded-[20px]  border-white">
+          <div className="text-black px-2 md:mt-2 rounded-t-[20px]  shadow-sm md:py-3 flex top-0 bg-white  items-center justify-between">
+            <h1 className=" font-bold text-[20px]">Cards</h1>
+            <FaPlus className="font-extralight text-gray-500 text-normal" />
+          </div>
+          <img src="/card.png" className=" h-[200px] object-contain" alt="" />
+        </div>
+        <div className="bg-white  rounded-[20px]  border-white">
+          <div className="text-black px-2 mt-2 rounded-t-[20px]  shadow-sm py-3 flex top-0 bg-white  items-center justify-between">
+            <h1 className=" font-bold text-[20px]">Scheduler</h1>
+            <FaPlus className="font-extralight text-gray-500 text-normal" />
+          </div>
+          <Calendar
+            value={selectedDay}
+            onChange={setSelectedDay}
+            shouldHighlightWeekends
+          />
+        </div>
+        <div className="bg-white  rounded-[20px]  border-white">
+          <div className="text-black px-2 mt-2 rounded-t-[20px]  shadow-sm py-3 flex top-0 bg-white  items-center justify-between">
+            <h1 className=" font-bold text-[20px]">Scheduler</h1>
+            <FaPlus className="font-extralight text-gray-500 text-normal" />
+          </div>
+          <Calendar
+            value={selectedDay}
+            onChange={setSelectedDay}
+            shouldHighlightWeekends
+          />
+        </div>
         <div className="bg-white h-[250px] rounded-[20px]  border-white"></div>
-        <div className="bg-white h-[250px] rounded-[20px]  border-white"></div>
-        <div className="bg-white h-[250px] rounded-[20px]  border-white"></div>
-        <div className="bg-white h-[250px] rounded-[20px]  border-white"></div>
-        <div className="bg-white h-[250px] rounded-[20px]  border-white"></div>
-        <div className="bg-white rounded-[20px] border-white md:col-span-2"></div>
-        {/* Add more divs as needed */}
       </div>
     </div>
   );
